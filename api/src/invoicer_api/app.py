758d38eac8833d808eff6c7d1de3959f4dc46985
Alex Mackechnie <mackechniealex@gmail.com>
2022-12-24 18:57:00 +0000

    Next iteration;

--- api/src/invoicer_api/app.py
+++ api/src/invoicer_api/app.py
@@ -1,7 +1,26 @@
-from flask import Flask
+from flask import Flask, request
+from invoicer_api.model.template import Template
+import sqlite3
+
+conn = sqlite3.connect("../db/invoicer.db")
+conn.cur
+
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "Hi there!"
+
+@app.route("/template", methods = ['GET', 'POST'])
+def create_template():
+    request_data = request.get_json()
+    template = Template(
+        request_data["template_name"],
+        request_data["name"],
+        request_data["address"],
+        request_data["payment_details"],
+        request_data["send_to"],
+        request_data["amount"],
+        request_data["description"],
+    )
+
+
+    return str(template.__dict__)
---
+++ api/src/invoicer_api/model/template.py
@@ -0,0 +1,10 @@
+class Template():
+    def __init__(self, template_name, name, address, payment_details, send_to, amount, description):
+        self.template_name = template_name
+        self.name = name
+        self.address = address
+        self.payment_details = payment_details
+        self.send_to = send_to
+        self.amount = amount
+        self.description = description
+
