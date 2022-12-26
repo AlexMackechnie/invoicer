a9e650c159983a0e4f05a810cc095ff9efff299c
Alex Mackechnie <mackechniealex@gmail.com>
2022-12-26 08:46:46 +0000

    GET and POST templates;

--- api/src/invoicer_api/app.py
+++ api/src/invoicer_api/app.py
@@ -2,14 +2,12 @@ from flask import Flask, request
 from invoicer_api.model.template import Template
 import sqlite3
 
-conn = sqlite3.connect("../db/invoicer.db")
-conn.cur
 
 
 app = Flask(__name__)
 
 
-@app.route("/template", methods = ['GET', 'POST'])
+@app.route("/template", methods = ['POST'])
 def create_template():
     request_data = request.get_json()
     template = Template(
@@ -22,5 +20,30 @@ def create_template():
         request_data["description"],
     )
 
+    conn = sqlite3.connect("../db/invoicer.db")
+    cur = conn.cursor()
 
+    cur.execute("INSERT INTO template (template_name, name, address, payment_details, send_to, amount, description) VALUES (?, ?, ?, ?, ?, ?, ?)", (template.template_name, template.name, template.address, template.payment_details, template.send_to, template.amount, template.description))
+
+    conn.commit()
+
+    return str(template.__dict__)
+
+
+@app.route("/template", methods = ['GET'])
+def get_template():
+    template_name = request.args.get("template_name")
+    conn = sqlite3.connect("../db/invoicer.db")
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM template WHERE template_name == '" + template_name + "';")
+    resp = cur.fetchall()
+
+    template = Template(*resp[0])
+
+    return str(template.__dict__)
+
+
+@app.route("/template", methods = ['POST'])
+def create_invoice_from_template():
+    request_data = request.get_json()
     return str(template.__dict__)
--- api/src/invoicer_api/model/template.py
+++ api/src/invoicer_api/model/template.py
@@ -1,10 +1,12 @@
-class Template():
-    def __init__(self, template_name, name, address, payment_details, send_to, amount, description):
-        self.template_name = template_name
-        self.name = name
-        self.address = address
-        self.payment_details = payment_details
-        self.send_to = send_to
-        self.amount = amount
-        self.description = description
+from dataclasses import dataclass
+
+@dataclass
+class Template:
+    template_name: str
+    name: str
+    address: str
+    payment_details: str
+    send_to: str
+    amount: float
+    description: str
 
